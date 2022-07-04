c0bc8c986abb14edce63c8a195762dcf14fb0bf8
Chris Owen <chris@thecodethatchriswrote.com>
2022-07-04 14:41:55 +0100

    Update testing.js

--- blackjack/support/testing.js
+++ blackjack/support/testing.js
@@ -1,5 +1,5 @@
 import { deck, playerTurn } from '../blackjack.js'
-import { stub } from 'https://deno.land/x/mock/stub.ts'
+import { stub } from 'https://deno.land/x/mock@0.13.0/stub.ts'
 import shuffle from './shuffle.js'
 
 export function playerChooses(choices) {
