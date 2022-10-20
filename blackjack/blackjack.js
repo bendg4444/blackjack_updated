42d8a7c043b2f12656f79e8658b7530d9c5280b4
bendg <bendg4444@gmail.com>
2022-10-20 17:39:20 +0100

    Almost working, requires compare function

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -9,27 +9,133 @@ const defaultLogger = await getDefaultLogger()
 
 export function generateDeck() {
   const cards = []
-
-  // TO DO: Write your code here
-
+  let suit = ''
+  let card = ''
+  for (let i = 0; i < 4; i++) {
+    //suit
+    if (i === 0) suit = 'S'
+    if (i === 1) suit = 'D'
+    if (i === 2) suit = 'C'
+    if (i === 3) suit = 'H'
+    for (let j = 0; j < 13; j++) {
+      //card
+      if (j === 0) card = 'A'
+      if (j === 1) card = '2'
+      if (j === 2) card = '3'
+      if (j === 3) card = '4'
+      if (j === 4) card = '5'
+      if (j === 5) card = '6'
+      if (j === 6) card = '7'
+      if (j === 7) card = '8'
+      if (j === 8) card = '9'
+      if (j === 9) card = '10'
+      if (j === 10) card = 'J'
+      if (j === 11) card = 'Q'
+      if (j === 12) card = 'K'
+      cards.push(card + suit)
+    }
+  }
   return cards
 }
 
-export function pointsFor(cards) {
-  // TO DO: Write your code here
+export function pointsForHand(hand) {
+  //count the points for the hand given
+
+  //goes through each card and calculates the score and returns the total
+  let points = hand.reduce((count, card) => {
+    count += pointsForCard(card[0])
+    return count
+  }, 0)
+  //total points for deck
+  return points
+}
+
+export function pointsForCard(card) {
+  //returns the number of points for a given card
+  let points = 0
+
+  //work out points for particular card
+  if (card[0] === 'A') {
+    points += 11 //ACE
+  } else if (card[0] === 'K' || card[0] === 'Q' || card[0] === 'J') {
+    points += 10 //FACE
+  } else if (card[0] === '1') {
+    points += 10 //10
+  } else {
+    points += parseInt(card[0])
+  } //Normal card (2->9)
+  //returns the number of points
+  return points
+}
+
+//function for player to draw card
+export function playerDrawsCard(deck, hand) {
+  let drawnCard = deck.shift()
+  defaultLogger.info('Hitting')
+  defaultLogger.info('You draw ' + drawnCard)
+  hand.push(drawnCard)
+  return hand
+}
+
+export function exitConditionMet(hand, player) {
+  //function to establish whether an exit condition is met
+  //return a true or false
+  //if true, process write out the correct end game response depending on player
+  if (pointsForHand(hand) === 21) {
+    //who win or lose
+    if (player === 'Dealer') {
+      defaultLogger.info(LOSE_MESSAGE)
+    } else if (player === 'Player') {
+      defaultLogger.info(WIN_MESSAGE)
+    }
+    return true
+  }
+  //two aces
+  if (hand.length <= 2) {
+    if (hand[0][0] === 'A' && hand[1][0] === 'A') {
+      //who win or lose
+      if (player === 'Dealer') {
+        defaultLogger.info(LOSE_MESSAGE)
+      } else if (player === 'Player') {
+        defaultLogger.info(WIN_MESSAGE)
+      }
+      return true
+    }
+  }
+  //number of cards = 6 or greater and less than equal to 21
+  if (hand.length >= 6 && pointsForHand(hand) <= 21) {
+    //who win or lose
+    if (player === 'Dealer') {
+      defaultLogger.info(LOSE_MESSAGE)
+    } else if (player === 'Player') {
+      defaultLogger.info(WIN_MESSAGE)
+    }
+    return true
+  } //greater than 21
+  if (pointsForHand(hand) > 21) {
+    if (player === 'Dealer') {
+      defaultLogger.info(WIN_MESSAGE)
+    } else if (player === 'Player') {
+      defaultLogger.info(LOSE_MESSAGE)
+    }
+    return true
+  }
+  //no win or lose condition met
+  return false
 }
 
 export function playerTurn(deck, hand, logger = defaultLogger) {
-  logger.info(`Your hand is ${hand.join(', ')}\n(${pointsFor(hand)} points)`)
-
   //Accept the choice from the player
   const action = window.prompt('What do you want to do? ("hit" or "stick")')
 
   switch (action) {
     case 'hit': {
       // TO DO: Draw a card
-
+      hand = playerDrawsCard(deck, hand)
       // It's still the player's turn
+      logger.info(
+        `Your hand is ${hand.join(', ')}\n(${pointsForHand(hand)} points)`
+      )
       return true
     }
     case 'stick': {
@@ -38,23 +144,80 @@ export function playerTurn(deck, hand, logger = defaultLogger) {
     }
     default: {
       // Unknown action
+      logger.info('Please enter correct command: ')
       break
     }
   }
 }
 
+//Plays game
 export function play({ seed = Date.now(), logger = defaultLogger } = {}) {
+  //ran once
   const newDeck = generateDeck()
   const shuffledDeck = shuffle(newDeck, seed)
   let playerHand = [shuffledDeck.shift(), shuffledDeck.shift()]
-
   let isPlayerTurn = true
 
-  while (isPlayerTurn) {
-    isPlayerTurn = playerTurn(shuffledDeck, playerHand, logger)
+  logger.info(
+    `Your hand is ${playerHand.join(', ')}\n(${pointsForHand(
+      playerHand
+    )} points)`
+  )
+
+  //End game if first cards provide winner
+  if (exitConditionMet(playerHand, 'Player')) {
+    return
   }
 
-  // TO DO: Dealer's turn
+  //Player makes a choice while still in the game e.g isPlayerTurn = true
+  while (isPlayerTurn) {
+    //player takes turn
+    isPlayerTurn = playerTurn(shuffledDeck, playerHand, logger)
+
+    //count the points after each player turn & check for win or lose
+    if (exitConditionMet(playerHand, 'Player')) {
+      return
+    }
+  }
+
+  let isDealersTurn = true
+  let dealersHand = [shuffledDeck.shift(), shuffledDeck.shift()]
+
+  logger.info(
+    `Dealer's hand is ${dealersHand.join(', ')}\n(${pointsForHand(
+      dealersHand
+    )} points)`
+  )
+  //dealer wins on drawn cards?
+  if (exitConditionMet(dealersHand, 'Dealer')) {
+    return
+  }
+  //dealer draws more than 17
+  if (pointsForHand(dealersHand) >= 17) {
+    logger.info('comparing scores')
+    //compare scores
+  } else {
+    //needs to draw more cards
+    while (isDealersTurn) {
+      //dealer draws a card
+      let drawnCard = shuffledDeck.shift()
+      logger.info('Dealer draws ' + drawnCard)
+      dealersHand.push(drawnCard)
+      logger.info(
+        `Dealer's hand is ${dealersHand.join(', ')}\n(${pointsForHand(
+          dealersHand
+        )} points)`
+      )
+      //checks whether win or lose
+      if (exitConditionMet(dealersHand, 'Dealer')) {
+        return
+      } //check whether loop has end
+      if (pointsForHand(dealersHand) >= 17) {
+        isDealersTurn = false
+      }
+    }
+    logger.info('comparing scores 2')
+  }
 }
 
 if (import.meta.main) {
--- blackjack/blackjack_student_tests.js
+++ blackjack/blackjack_student_tests.js
@@ -1,6 +1,150 @@
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts'
 // import { something } from './blackjack.js'
+import {
+  exitConditionMet,
+  generateDeck,
+  play,
+  playerDrawsCard,
+  pointsForCard,
+  pointsForHand
+} from './blackjack.js'
+import shuffle from './support/shuffle.js'
+import { getTestLogger } from './support/logging.ts'
 
-Deno.test("some test you've written", () => {
-  assertEquals(2, 1 + 1)
+const deck1 = ['2H', 'AS'] //13
+const deck2 = ['JH', 'JD', 'QD'] //30
+const deck3 = ['2S', '5C', 'AC', 'KD'] //28
+const deck4 = ['AS', 'AC'] //21
+const deck5 = ['KS', '5H', '6C'] //21
+const deck6 = ['2H', '3D', '4D', '2D', '3S', '7S']
+
+Deno.test("generateDeck(): a fresh deck in 'new deck order'", () => {
+  // prettier-ignore
+  assertEquals(generateDeck(), [
+    'AS',
+    '2S',
+    '3S',
+    '4S',
+    '5S',
+    '6S',
+    '7S',
+    '8S',
+    '9S',
+    '10S',
+    'JS',
+    'QS',
+    'KS',
+    'AD',
+    '2D',
+    '3D',
+    '4D',
+    '5D',
+    '6D',
+    '7D',
+    '8D',
+    '9D',
+    '10D',
+    'JD',
+    'QD',
+    'KD',
+    'AC',
+    '2C',
+    '3C',
+    '4C',
+    '5C',
+    '6C',
+    '7C',
+    '8C',
+    '9C',
+    '10C',
+    'JC',
+    'QC',
+    'KC',
+    'AH',
+    '2H',
+    '3H',
+    '4H',
+    '5H',
+    '6H',
+    '7H',
+    '8H',
+    '9H',
+    '10H',
+    'JH',
+    'QH',
+    'KH'
+  ])
 })
+
+Deno.test(
+  'playerDrawsCard(): player draws a card and adds to deck',
+  async () => {
+    const { logger, handler } = await getTestLogger()
+    const newDeck = generateDeck()
+    const seed = 12
+    let shuffledDeck = shuffle(newDeck, seed)
+    let playerHand = [shuffledDeck.shift(), shuffledDeck.shift()]
+
+    //draws once
+    playerHand = playerDrawsCard(shuffledDeck, playerHand) //JS
+    //Draws KS
+    assertEquals(playerDrawsCard(shuffledDeck, playerHand), [
+      '2H',
+      'KC',
+      'JS',
+      'KH'
+    ])
+  }
+)
+
+Deno.test(
+  'pointsForCard(): Testing the right score for the selected card',
+  async () => {
+    const { logger, handler } = await getTestLogger()
+
+    //Cards
+    const card1 = '2H'
+    const card2 = '10D'
+    const card3 = 'KS'
+    const card4 = 'AD'
+
+    assertEquals(pointsForCard(card1), 2)
+    assertEquals(pointsForCard(card2), 10)
+    assertEquals(pointsForCard(card3), 10)
+    assertEquals(pointsForCard(card4), 11)
+  }
+)
+
+Deno.test(
+  'pointsForHand(): Testing the right amount of points returned for deck of cards',
+  async () => {
+    assertEquals(pointsForHand(deck1), 13)
+    assertEquals(pointsForHand(deck2), 30)
+    assertEquals(pointsForHand(deck3), 28)
+    assertEquals(pointsForHand(deck4), 22)
+  }
+)
+
+Deno.test(
+  'exitConditionMet(): Testing whether an exitConditionMet returns true or false for values',
+  async () => {
+    //13, not bust
+    assertEquals(exitConditionMet(deck1, 'Dealer'), false)
+    assertEquals(exitConditionMet(deck1, 'Player'), false)
+    //30, bust
+    assertEquals(exitConditionMet(deck2, 'Dealer'), true)
+    assertEquals(exitConditionMet(deck2, 'Player'), true)
+    //28, bust
+    assertEquals(exitConditionMet(deck3, 'Dealer'), true)
+    assertEquals(exitConditionMet(deck3, 'Player'), true)
+    //2 aces, 21
+    assertEquals(exitConditionMet(deck4, 'Dealer'), true)
+    assertEquals(exitConditionMet(deck4, 'Player'), true)
+    //21, win
+    assertEquals(exitConditionMet(deck5, 'Dealer'), true)
+    assertEquals(exitConditionMet(deck5, 'Player'), true)
+    //6cards, less than 21
+    assertEquals(exitConditionMet(deck6, 'Dealer'), true)
+    assertEquals(exitConditionMet(deck6, 'Player'), true)
+  }
+)
